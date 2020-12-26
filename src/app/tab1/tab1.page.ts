1c72338e95b080d28c9ab689fdbfba001f8d8231
t-cool <hiroki802@hotmail.com>
2020-12-26 16:16:21 +0900

    canvas を使う環境を設定

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,6 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-
-import { PieBoxComponent } from '../pie-box/pie-box.component'
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-tab1',
@@ -10,12 +8,22 @@ import { PieBoxComponent } from '../pie-box/pie-box.component'
 
 export class Tab1Page implements OnInit{
 
-  @ViewChild(PieBoxComponent) pieBox: PieBoxComponent;
-
   constructor(
   ) {}
 
+  // canvas の環境設定
+  @ViewChild('canvas', { static: true })
+  canvas: ElementRef<HTMLCanvasElement>;
+  ctx: any;
+
   ngOnInit(){
+    this.ctx = this.canvas.nativeElement.getContext('2d');
+    this.ctx.fillStyle = 'red';
+    this.ctx.strokeStyle = "red" ;
+    this.ctx.beginPath();
+    this.ctx.moveTo(0,0);
+    this.ctx.lineTo(50, 200);
+    this.ctx.stroke();
   }
 
 }
