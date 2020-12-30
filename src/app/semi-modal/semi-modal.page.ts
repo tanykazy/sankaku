1cbee5f2339d70a39cc906d41379e5815f1861da
t-cool <hiroki802@hotmail.com>
2020-12-30 14:01:36 +0900

    ダイアログボックスのデザインを実装

--- src/app/semi-modal/semi-modal.page.ts
+++ src/app/semi-modal/semi-modal.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Inject, Input } from '@angular/core';
 
 @Component({
   selector: 'app-semi-modal',
@@ -11,9 +11,44 @@ export class SemiModalPage implements OnInit {
   @Input() lastName: string;
   @Input() middleInitial: string;
 
+  // canvas の環境設定
+  @ViewChild('canvas', { static: true })
+  canvas: ElementRef<HTMLCanvasElement>;
+
+  ctx: any;
+
   constructor() { }
 
   ngOnInit() {
+    // canvas
+    this.ctx = this.canvas.nativeElement.getContext('2d');
+    // 長さ6点分
+    this.ctx.beginPath();
+    this.ctx.moveTo(150,0);
+    this.ctx.lineTo(150, 300);
+    this.ctx.strokeStyle = "red" ;
+    this.ctx.lineWidth = 4;
+    this.ctx.lineCap = "round";
+    this.ctx.stroke();
+    this.ctx.closePath(); 
+
+    // 長さ3点分
+    this.ctx.beginPath();
+    this.ctx.moveTo(150, 300);
+    this.ctx.lineTo(0, 150);
+    this.ctx.lineWidth = 4;
+    this.ctx.lineCap = "round";
+    this.ctx.strokeStyle = "green" ;
+    this.ctx.stroke();
+    // 長さ3点分
+    this.ctx.beginPath();
+    this.ctx.moveTo(0, 150);
+    this.ctx.lineTo(150, 0);
+    this.ctx.lineWidth = 4;
+    this.ctx.lineCap = "round";
+    this.ctx.strokeStyle = "green" ;
+    this.ctx.stroke();
+
   }
 
 }
