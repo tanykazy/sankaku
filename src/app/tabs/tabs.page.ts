53b6260642fe0788f129f791b13c97664bd74001
t-cool <hiroki802@hotmail.com>
2020-12-30 15:17:33 +0900

    canvas サイズを200pxに変更。分類用の場所を３箇所作成

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { SemiModalPage } from '../semi-modal/semi-modal.page';
 
 @Component({
   selector: 'app-tabs',
@@ -7,6 +9,24 @@ import { Component } from '@angular/core';
 })
 export class TabsPage {
 
-  constructor() {}
+  constructor(
+    public modalController: ModalController
+  ) {}
+
+  async presentModal() {
+    const modal = await this.modalController.create({
+      component: SemiModalPage,
+      cssClass: 'my-custom-class', // global.scss に記入する必要あり 
+      componentProps: {
+        /* 
+        'firstName': 'GDG',
+        'lastName': 'Nara',
+        'middleInitial': 'EdTech'
+        */
+      }
+    });
+    return await modal.present();
+  }
+
 
 }
