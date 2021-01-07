19503279ec165ec7b64503e4fd7da3f68691fdfd
t-cool <hiroki802@hotmail.com>
2021-01-07 21:46:15 +0900

    不要なフォルダやファイルを削除

--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab1Page } from './tab1.page';
-import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 
 import { Tab1PageRoutingModule } from './tab1-routing.module';
 
@@ -14,7 +13,6 @@ import {DragDropModule} from '@angular/cdk/drag-drop';
     IonicModule,
     CommonModule,
     FormsModule,
-    ExploreContainerComponentModule,
     Tab1PageRoutingModule,
     DragDropModule
   ],
